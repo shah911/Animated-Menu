3f7b644976e64b485fb9a7e98fc91357aeaf98df
shah 911 <mianshahzeb911@gmail.com>
2024-07-05 10:08:32 +0500

    height and padding adjustment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between mx-auto w-[85%] md:w-[90%] py-10 2xl:py-[3vw]">
+    <div className="flex items-center justify-between mx-auto w-[85%] md:w-[90%] py-6 2xl:py-[2vw]">
       <Link
         href="/"
         className="z-20 font-medium tracking-tighter text-3xl 2xl:text-[2.25vw]"
